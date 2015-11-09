b8f91c69b433d777d84b2090d32144acf2a64108
capitaladot <capitaladot@users.noreply.github.com>
2015-11-09 12:43:44 -0500

    Logging on exception.

--- src/MartinBean/MenuBuilder/MenuItem.php
+++ src/MartinBean/MenuBuilder/MenuItem.php
@@ -1,34 +1,40 @@
-<?php namespace MartinBean\MenuBuilder;
+<?php
+
+namespace MartinBean\MenuBuilder;
 
 use Illuminate\Database\Eloquent\Model;
+use Symfony\Component\Debug\Exception\FatalErrorException;
 
 class MenuItem extends Model {
-
 	protected $table = 'menu_items';
-
-	protected $fillable = [
-		'menu_id',
-		'sort_order',
+	protected $fillable = [ 
+			'menu_id',
+			'sort_order' 
 	];
-
-	public function navigatable()
-	{
-		return $this->morphTo();
+	public function navigatable() {
+		return $this->morphTo ();
 	}
-
-	public function menu()
-	{
-		return $this->belongsTo('MartinBean\MenuBuilder\Menu');
+	public function menu() {
+		return $this->belongsTo ( 'MartinBean\MenuBuilder\Menu' );
 	}
-
-	public function getTitle()
-	{
-		return $this->navigatable->getTitle();
+	public function getTitle() {
+		if ($this->navigatable)
+			try {
+				return $this->navigatable->getTitle ();
+			} catch ( Symfony\Component\Debug\Exception\FatalErrorException $e ) {
+				Log::critical ( 'MenuItem exception', [ 
+						$e 
+				] );
+			}
 	}
-
-	public function getUrl()
-	{
-		return $this->navigatable->getUrl();
+	public function getUrl() {
+		if ($this->navigatable)
+			try {
+				return $this->navigatable->getUrl ();
+			} catch ( Symfony\Component\Debug\Exception\FatalErrorException $e ) {
+				Log::critical ( 'MenuItem exception', [ 
+						$e 
+				] );
+			}
 	}
-
 }
